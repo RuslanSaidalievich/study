37b845119e0d7100418522d36c668fc573f759f0
Ruslanyo <ruslan.saidalievich.tsugaev@gmail.com>
2021-02-09 21:57:27 +0300

    clean code after 3rd lesson functions

--- js/script.js
+++ js/script.js
@@ -36,39 +36,6 @@ function rememberMyFilms () {
 
 rememberMyFilms();
 
-
-// WHILE
-// let i = 0;
-// while (i < 2) {
-
-//     const a = prompt('Один из последних просмотренных фильмов?', ''),
-//           b = prompt('На сколько оцените его?', '');
-//     i++;
-//     if (a != 0 && b != null && a != '' && b != '' && a.length < 50) {
-//             personalMovieDB.movies[a] = b;
-//             console.log('Done');
-//         } else {
-//             console.log('Error');
-//             i--;
-//         }   
-// }
-
-
-// DO
-// let i = 0;
-// do {
-//     const a = prompt('Один из последних просмотренных фильмов?', ''),
-//         b = prompt('На сколько оцените его?', '');
-//     i++;
-//     if (a != 0 && b != null && a != '' && b != '' && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('Done');
-//     } else {
-//         console.log('Error');
-//         i--;
-//     }
-// } while (i<2);
-
 function detectPersonalLevel () {
     if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
         console.log('Просмотренно мало фильмов');
@@ -82,12 +49,6 @@ function detectPersonalLevel () {
 }
 
 detectPersonalLevel();
-
-// function showMyDB () {
-//     if (personalMovieDB.privat == false) {
-//         console.log(personalMovieDB);
-//     }
-// }
 
 function showMyDB (hidden) {
     if (!hidden) {
