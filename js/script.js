6b6c057de5b846002738b092ed5ff17917f66269
Ruslanyo <ruslan.saidalievich.tsugaev@gmail.com>
2021-02-09 16:48:27 +0300

    Lesson loops modified

--- js/script.js
+++ js/script.js
@@ -22,11 +22,43 @@ for (let i = 0; i < 2; i++) {
     }   
 }
 
+// WHILE
+// let i = 0;
+// while (i < 2) {
+
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+//     i++;
+//     if (a != 0 && b != null && a != '' && b != '' && a.length < 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log('Done');
+//         } else {
+//             console.log('Error');
+//             i--;
+//         }   
+// }
+
+
+// DO
+// let i = 0;
+// do {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+//     i++;
+//     if (a != 0 && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('Done');
+//     } else {
+//         console.log('Error');
+//         i--;
+//     }
+// } while (i<2);
+
 if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
     console.log('Просмотренно мало фильмов');
-} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
     console.log('Просмотренно много фильмов');
-} else if (personalMovieDB.count > 30) {
+} else if (personalMovieDB.count >= 30) {
     console.log('Вы киноман');
 } else {
     console.log('Произошла ошибка');
